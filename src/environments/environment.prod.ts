ba2c5c377fa4703c71783dc02e6a1224aff6d2d6
abhirajwagh <waghabhiraj@gmail.com>
2021-06-18 15:24:06 +0530

    add values in prod environment ts file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  DefaultLanguage: 'en-US',
+  LaundryIroningAPIUrl: 'https://localhost:44389/api/',
+  IroningRate: 7,
+  LaundryRate: 42,
+  IroningLaundryRate: 91
 };
